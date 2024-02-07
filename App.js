7d8d5cd1f83470e7a8b95dcb61007c2e2c8a85e9
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-02-07 09:38:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text, StatusBar } from "react-native";
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="marrow" />
+      <StatusBar barStyle="light-content" backgroundColor="blue" />
       <View>
         <Text>Olá React Native!</Text>
       </View>
