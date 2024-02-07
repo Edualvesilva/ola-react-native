1be688a8b4d9affcae1c81a170bbb9559822d1d9
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-02-07 09:25:46 -0300

    StatusBar

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View>
+        <Text>Olá React Native!</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
